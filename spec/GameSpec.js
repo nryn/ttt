42f3ea73258d8a8476c4c08f9a70db52c35a61be
nryn <narayanohanlon@gmail.com>
2017-04-19 16:24:32 +0100

    game understands vertical and horizontal win combinations

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,14 +4,50 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("should have an empty map of the game to begin", function() {
-    expect(game.map).toEqual(
+  it("should have an empty game board to begin", function() {
+    expect(game.board).toEqual(
       {
-        topRow: [,,],
-        midRow: [,,],
-        botRow: [,,]
+        topRow: ["","",""],
+        midRow: ["","",""],
+        botRow: ["","",""]
       }
     );
+  });
+
+  it("should be able to check for horizontal wins", function() {
+    game.board = {
+      topRow: ["","","O"],
+      midRow: ["X","X","X"],
+      botRow: ["","O",""]
+    }
+    expect(game.isWinHorizontal()).toBe(true);
+  });
+
+  it("should be able to check for horizontal non-wins", function() {
+    game.board = {
+      topRow: ["","","O"],
+      midRow: ["X","O","X"],
+      botRow: ["","O",""]
+    }
+    expect(game.isWinHorizontal()).toBe(false);
+  });
+
+  it("should be able to check for vertical wins", function() {
+    game.board = {
+      topRow: ["X","X","O"],
+      midRow: ["O","X","O"],
+      botRow: ["X","O","O"]
+    }
+    expect(game.isWinVertical()).toBe(true);
+  });
+
+  it("should be able to check for vertical non-wins", function() {
+    game.board = {
+      topRow: ["O","X","O"],
+      midRow: ["X","O","X"],
+      botRow: ["O","O","X"]
+    }
+    expect(game.isWinVertical()).toBe(false);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,43 @@
 function Game() {
-  this.map = {
-    topRow: [,,],
-    midRow: [,,],
-    botRow: [,,]
+  this.board = {
+    topRow: ["","",""],
+    midRow: ["","",""],
+    botRow: ["","",""]
   }
 };
+
+Game.prototype.isWinHorizontal = function() {
+  var that = this;
+  var result = false;
+  Object.keys(this.board).forEach(function(row) {
+    if (isWinningCombo(that.board[row]))
+      result = true;
+  });
+  return result;
+};
+
+
+Game.prototype.isWinVertical = function() {
+  var that = this;
+  var columns = [[],[],[]];
+  Object.keys(this.board).forEach( function(row){
+    for (var i = 0; i<3; i++) {
+      columns[i].push(that.board[row][i]);
+    };
+  });
+  var result = false;
+  columns.forEach(function(column) {
+    if (isWinningCombo(column))
+      result = true;
+  });
+  return result;
+};
+
+
+function isWinningCombo(arr){
+   for (var i = 1; i<3; i++) {
+      if (arr[i] === "" || arr[i] !== arr[i-1])
+         return false;
+   }
+   return true;
+}
